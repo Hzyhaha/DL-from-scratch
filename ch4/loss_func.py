d87d192e2837f139250433b89f059956c9049f91
Zhengyu Hu <huzhengyu621@gmail.com>
2024-10-29 15:41:09 +0800

    add loss_func

---
+++ ch4/loss_func.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+def mean_squared_error(y,t):
+    '''
+    :param y: the output of the NN
+    :param t: the label of the input
+    '''
+    return 0.5*np.sum((y-t)**2)
+
+def cross_entropy_error(y,t):
+    '''
+    :param y: the output of the NN
+    :param t: the label of the input
+    '''
+    delta = 1e-7
+    return -np.sum(t*np.log(y+delta))
