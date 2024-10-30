496fd5622556763ed5930fb89e6aeb4394dbb8f5
Zhengyu Hu <huzhengyu621@gmail.com>
2024-10-30 15:41:53 +0800

    Load MNIST by torchvision. Calculate the loss function of minibatch.

--- ch4/loss_func.py
+++ ch4/loss_func.py
@@ -14,3 +14,15 @@ def cross_entropy_error(y,t):
     '''
     delta = 1e-7
     return -np.sum(t*np.log(y+delta))
+
+def minibatch_cross_entropy_error(y,t):
+    '''
+    :param y: the output of the NN
+    :param t: the label (one-hot) of the input 
+    '''
+    delta = 1e-7
+    if y.ndim == 1:
+        y = y.reshape(1,-1)
+        t = t.reshape(1,-1)
+    batch_size = y.shape[0]
+    return -np.sum(t*np.log(y+delta))/batch_size
