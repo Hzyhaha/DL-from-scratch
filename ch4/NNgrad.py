73e586285e08c1e425fa140a8a782169b54fcf86
Zhengyu Hu <huzhengyu621@gmail.com>
2024-11-02 19:11:54 +0800

    改进了计算梯度的方法，实现了神经网络梯度的计算

---
+++ ch4/NNgrad.py
@@ -0,0 +1,39 @@
+import numpy as np
+from loss_func import cross_entropy_error
+from diffraction import numerical_diff
+from grad_desc import gradient_descent
+
+def softmax(x):
+    C = np.max(x)
+    tmp = np.exp(x-C)
+    return tmp/np.sum(tmp)
+
+class simple_net:
+    def __init__(self) -> None:
+        self.weight = np.random.random([2,3])
+
+    def predict(self,x):
+        return x@self.weight
+    
+    def loss(self,x,t):
+        z = self.predict(x)
+        return cross_entropy_error(softmax(z),t)
+    
+
+
+
+if __name__ == '__main__':
+    net = simple_net()
+    print(net.weight)
+    x = np.array([0.6, 0.9])
+    print(net.predict(x))
+    t = np.array([0.0,1.0,0.0])
+    print(net.loss(x,t))
+    def f(w):
+        return net.loss(x,t)
+    dw = numerical_diff(f,net.weight)
+    print(dw)
+
+    gradient_descent(f,net.weight,lr=0.01,num_step=1000)
+
+    print('final loss: ',net.loss(x,t),'probability: ',softmax(net.predict(x)))
--- ch4/diffraction.py
+++ ch4/diffraction.py
@@ -13,8 +13,10 @@ def numerical_diff(f,x:np.ndarray):
 
     h = 1e-4
     grad = np.zeros_like(x)
-    
-    for i in range(x.size):
+    # 多维矩阵利用索引迭代
+    it = np.nditer(x,flags=['multi_index'])
+    while not it.finished:
+        i = it.multi_index
         tmp = x[i]
         x[i] = tmp + h
         forward = f(x)
@@ -24,11 +26,29 @@ def numerical_diff(f,x:np.ndarray):
 
         x[i] = tmp
         grad[i] = (forward - back)/2/h
+        it.iternext()
     return grad
 
+'''    for i in range(x.size):
+        tmp = x[i]
+        x[i] = tmp + h
+        forward = f(x)
+
+        x[i] = tmp - h
+        back = f(x)
+
+        x[i] = tmp
+        grad[i] = (forward - back)/2/h
+   return grad
+'''
 def func1(x):
     return x[0]**2+x[1]**2
 
 if __name__=='__main__':
     print(numerical_diff(func1,np.array([3.0,0.0])))
+
+
+    x = np.array([[2.0,3.0,1.0],[2.0,0.0,1.0]])
+    print(x.size)
+    print(x[0])
     # 整数会出现精度误差
