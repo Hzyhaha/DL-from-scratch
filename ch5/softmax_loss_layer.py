831f8c881b34d986df114d2bfadc8ec4a3f0a1e4
Zhengyu Hu <huzhengyu621@gmail.com>
2024-11-11 16:44:52 +0800

    完成softmax with loss 层的编写 problem: 不知道为何要除batch size.

---
+++ ch5/softmax_loss_layer.py
@@ -0,0 +1,28 @@
+import numpy as np
+import sys
+sys.path.append('.')
+from common.activation_func import softmax
+from common.loss_func import cross_entropy_error
+
+class Softmax_Loss_Layer:
+    def __init__(self) -> None:
+        self.loss = None
+        self.y = None
+        self.t = None
+
+    def forward(self,x,t):
+        self.y = softmax(x)
+        self.t = t
+        self.loss = cross_entropy_error(self.y,self.t)
+        return self.loss
+    
+    def backward(self,dout=1):
+        batch_size = self.t.shape[0]
+        return (self.y - self.t)*dout/batch_size
+
+if __name__ == '__main__':
+    x = np.array([1,2,1])
+    t = np.array([0,1,0])
+    testlayer = Softmax_Loss_Layer()
+    testlayer.forward(x,t)
+    print(testlayer.y,testlayer.loss,testlayer.backward())
